f932b855f5f03d386b4d26536dcbd633db21378b
steepboy <nikbustrov@gmail.com>
2024-10-14 23:42:27 +0300

    add 404 to resolv

--- skin_system/resolv.py
+++ skin_system/resolv.py
@@ -6,6 +6,6 @@ def resolve(url):
     response = requests.get(url)
 
     if response.status_code != 200:
-        return {}
+        return {}, 404
 
     return Response(response.content, content_type=response.headers['Content-Type'])
